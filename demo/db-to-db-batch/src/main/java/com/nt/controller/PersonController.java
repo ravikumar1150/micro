8b732d7404705dc3461824cdaca2adcdc8fa09f3
ravikumar <ravikumark1150@gmail.com>
2025-06-24 18:42:06 +0530

    sysout message add

--- demo/db-to-db-batch/src/main/java/com/nt/controller/PersonController.java
+++ demo/db-to-db-batch/src/main/java/com/nt/controller/PersonController.java
@@ -1,5 +1,6 @@
 package com.nt.controller;
 
+import com.nt.DbToDbBatchApplication;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
@@ -10,9 +11,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+
 @RestController
 @RequestMapping("/p")
 public class PersonController {
+
+    private final DataSource dataSource;
+
+    private final DbToDbBatchApplication dbToDbBatchApplication;
 
     @Autowired
     private JobLauncher jobLauncher;
@@ -20,9 +27,16 @@ public class PersonController {
     @Autowired
     private Job job;
 
+    PersonController(DbToDbBatchApplication dbToDbBatchApplication, DataSource dataSource) {
+        this.dbToDbBatchApplication = dbToDbBatchApplication;
+        this.dataSource = dataSource;
+    }
+
     @GetMapping("/job")
     public String startJob() {
+    	System.out.print("pathc recor");
         try {
+        
             JobParameters jobParameters = new JobParametersBuilder()
                     .addLong("time", System.currentTimeMillis())
                     .toJobParameters();
